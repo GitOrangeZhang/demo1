33334d7247694db5f1b971f411405c01c5e8886e
chengzhang <120496649@qq.com>
2019-04-30 23:31:41 +0800

    lambda语法精简

---
+++ src/syntax/Syntax4.java
@@ -0,0 +1,30 @@
+package syntax;
+
+import data.Person;
+
+/**
+ * @author:ChengZhang
+ * @date: 2019/4/30
+ * @time: 23:17
+ */
+public class Syntax4 {
+    public static void main(String[] args) {
+
+        PersonCreater creater = () -> new Person();
+
+        //构造方法引用
+        PersonCreater creater1 = Person::new;
+        Person a = creater1.getPerson();
+
+        PersdonCreater2 creater2 = Person::new;
+        Person b = creater2.getPerson("fuck",18);
+    }
+}
+
+interface PersonCreater{
+    Person getPerson();
+}
+
+interface PersdonCreater2{
+    Person getPerson(String name,int age);
+}
